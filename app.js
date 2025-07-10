b11ff1a1ea5cd94c0170dbdad19280e5fcd94376
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-10 16:47:43 -0300

    feat: reorganize app.js to properly set up static files and routes

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const productsRouter = require("./src/routes/productsRouter");
 
 app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", (req, res) => {
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
   res.render("home", { title: "Homepage" });
 });
+
+app.use("/products", productsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

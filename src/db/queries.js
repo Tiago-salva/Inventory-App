b22ad6de4ad8d4e1b59fd3fad1f3d5c34a84158c
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-11 18:06:35 -0300

    feat: add function to retrieve a single product by ID

--- src/db/queries.js
+++ src/db/queries.js
@@ -5,4 +5,11 @@ async function getAllProducts() {
   return rows;
 }
 
-module.exports = { getAllProducts };
+async function getProduct(id) {
+  const { rows } = await pool.query(`SELECT * FROM products WHERE id = $1`, [
+    id,
+  ]);
+  return rows[0];
+}
+
+module.exports = { getAllProducts, getProduct };

9240115ee9bdbf12a76db1d2e87b13b9a8191477
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-12 14:51:32 -0300

    feat: add routes for creating new products

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,10 +2,15 @@ const { Router } = require("express");
 const {
   getAllProducts,
   getProduct,
+  createProductGet,
+  createProductPost,
 } = require("../controllers/productsController");
+
 const productsRouter = Router();
 
 productsRouter.get("/", getAllProducts);
 productsRouter.get("/product/:id", getProduct);
+productsRouter.get("/new", createProductGet);
+productsRouter.post("/new", createProductPost);
 
 module.exports = productsRouter;

3869856188a93bde9af979313c38da93a5650619
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-12 13:38:36 -0300

    feat: add functions to retrieve all types of clothes and brands, and insert a new product

--- src/db/queries.js
+++ src/db/queries.js
@@ -12,4 +12,36 @@ async function getProduct(id) {
   return rows[0];
 }
 
-module.exports = { getAllProducts, getProduct };
+async function getAllTypeClothes() {
+  const { rows } = await pool.query(`SELECT * FROM type_clothes`);
+  return rows;
+}
+
+async function getAllBrands() {
+  const { rows } = await pool.query(`SELECT * FROM brands`);
+  return rows;
+}
+
+async function insertProduct(product) {
+  await pool.query(
+    `INSERT INTO products (type_clothes_id, brand_id, product_name, product_description, price, image_url, quantity)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+    [
+      parseInt(product.typeClothes),
+      parseInt(product.brand),
+      product.productName,
+      product.description,
+      product.price,
+      product.imgUrl,
+      product.quantity,
+    ]
+  );
+}
+
+module.exports = {
+  getAllProducts,
+  getProduct,
+  getAllTypeClothes,
+  getAllBrands,
+  insertProduct,
+};

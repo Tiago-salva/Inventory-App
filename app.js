cf8ff239e35440d52e0d1f084e3140fd6dab7472
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-12 19:33:05 -0300

    feat: add categories and brands routes to the app

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const productsRouter = require("./src/routes/productsRouter");
+const categoriesRouter = require("./src/routes/categoriesRouter");
+const brandsRouter = require("./src/routes/brandsRouter");
 
 app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
@@ -14,6 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+app.use("/brands", brandsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

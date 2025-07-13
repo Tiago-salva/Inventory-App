9ebf3d00bc1c86d4cb7e745eb56bf42de2760377
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-13 19:38:00 -0300

    feat: add functions to render a 'create' form and create a category

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -8,14 +8,26 @@ async function getAllCategories(req, res) {
 async function getAllProductsByType(req, res) {
   const id = parseInt(req.params.id);
   const allProducts = await db.getAllProductsBy("type", id);
-  console.log(allProducts);
+  if (allProducts.length < 1) return res.send("There are no products");
   res.render("products", {
     title: `All the ${allProducts[0].type_name}`,
     products: allProducts,
   });
 }
 
+async function createCategoryGet(req, res) {
+  res.render("form", { action: "categories", name: "category" });
+}
+
+async function createCategoryPost(req, res) {
+  const categoryName = req.body.category;
+  await db.insertIn("type_clothes", categoryName);
+  res.redirect("/categories");
+}
+
 module.exports = {
   getAllCategories,
   getAllProductsByType,
+  createCategoryGet,
+  createCategoryPost,
 };

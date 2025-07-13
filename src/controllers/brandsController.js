03f383f702ff1e6d0f9569198977af9ba2e4f086
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-13 19:37:40 -0300

    feat: add functions to render a 'create' form and create a brand

--- src/controllers/brandsController.js
+++ src/controllers/brandsController.js
@@ -8,13 +8,26 @@ async function getAllBrands(req, res) {
 async function getAllProductsByBrand(req, res) {
   const id = parseInt(req.params.id);
   const allProducts = await db.getAllProductsBy("brand", id);
+  if (allProducts.length < 1) return res.send("There are no products");
   res.render("products", {
     title: `All of ${allProducts[0].brand_name}`,
     products: allProducts,
   });
 }
 
+async function createBrandGet(req, res) {
+  res.render("form", { action: "brands", name: "brand" });
+}
+
+async function createBrandPost(req, res) {
+  const brandName = req.body.brand;
+  await db.insertIn("brands", brandName);
+  res.redirect("/brands");
+}
+
 module.exports = {
   getAllBrands,
   getAllProductsByBrand,
+  createBrandGet,
+  createBrandPost,
 };

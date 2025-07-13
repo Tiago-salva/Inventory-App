fbc25aea2ad70bca788c5462f5a921d5bd11be21
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-13 19:39:38 -0300

    feat: add 'new' routes to handle creation of brands

--- src/routes/brandsRouter.js
+++ src/routes/brandsRouter.js
@@ -2,10 +2,14 @@ const { Router } = require("express");
 const {
   getAllProductsByBrand,
   getAllBrands,
+  createBrandGet,
+  createBrandPost,
 } = require("../controllers/brandsController");
 const brandsRouter = Router();
 
 brandsRouter.get("/", getAllBrands);
+brandsRouter.get("/new", createBrandGet);
+brandsRouter.post("/new", createBrandPost);
 brandsRouter.get("/:id", getAllProductsByBrand);
 
 module.exports = brandsRouter;

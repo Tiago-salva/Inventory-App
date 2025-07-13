c63882179e6012e5a7011607c2211dc8ac36a02d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-13 19:39:50 -0300

    feat: add 'new' routes to handle creation of categories

--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -2,10 +2,14 @@ const { Router } = require("express");
 const {
   getAllCategories,
   getAllProductsByType,
+  createCategoryGet,
+  createCategoryPost,
 } = require("../controllers/categoriesController");
 const categoriesRouter = Router();
 
 categoriesRouter.get("/", getAllCategories);
+categoriesRouter.get("/new", createCategoryGet);
+categoriesRouter.post("/new", createCategoryPost);
 categoriesRouter.get("/:id", getAllProductsByType);
 
 module.exports = categoriesRouter;
